9d62e5e4c33b482ea869cd88ea7054fbce0b9650
Brad Ben Joya <bradbenjoya6@gmail.com>
2026-01-19 21:43:01 +0200

    Created a basic config with the vauge colorscheme and support for language server.

---
+++ init.lua
@@ -0,0 +1,20 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true§
+vim.g.mapleader = " "
+
+vim.pack.add({
+  {src = "https://github.com/vague2k/vague.nvim"},
+  {src = "https://github.com/stevearc/oil.nvim"},
+  {src = "https://github.com/echasnovski/mini.pick"},
+  {src = "https://github.com/neovim/nvim-lspconfig"},
+  {src = "https://github.com/chomosuke/typst-preview.nvim"},
+})
+
+vim.lsp.enable({"clangd"})
+vim.lsp.enable({"lua_ls"})
+vim.cmd("colorscheme vague")
+vim.cmd(":hi statusline guibg=NONE")
