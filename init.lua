86bc54cb17ce63840a696b14b70292005eb277e8
Brad Ben Joya <bradbenjoya6@gmail.com>
2026-01-19 21:59:21 +0200

    Added ultimate-autopair for easy brackets completion.

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
-vim.opt.expandtab = true§
+vim.opt.expandtab = true
 vim.g.mapleader = " "
 
 vim.pack.add({
@@ -12,9 +12,13 @@ vim.pack.add({
   {src = "https://github.com/echasnovski/mini.pick"},
   {src = "https://github.com/neovim/nvim-lspconfig"},
   {src = "https://github.com/chomosuke/typst-preview.nvim"},
+	{src = "https://github.com/altermo/ultimate-autopair.nvim"},
 })
 
 vim.lsp.enable({"clangd"})
 vim.lsp.enable({"lua_ls"})
+
+require'ultimate-autopair'.setup{}
+
 vim.cmd("colorscheme vague")
 vim.cmd(":hi statusline guibg=NONE")
