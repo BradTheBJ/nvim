54173394f6405ea6ac5addaddc656728df47cf9f
Brad Ben Joya <bradbenjoya6@gmail.com>
2026-01-21 18:08:21 +0200

    Added Zig language server.

--- init.lua
+++ init.lua
@@ -7,18 +7,55 @@ vim.opt.expandtab = true
 vim.g.mapleader = " "
 
 vim.pack.add({
-  {src = "https://github.com/vague2k/vague.nvim"},
-  {src = "https://github.com/stevearc/oil.nvim"},
-  {src = "https://github.com/echasnovski/mini.pick"},
-  {src = "https://github.com/neovim/nvim-lspconfig"},
-  {src = "https://github.com/chomosuke/typst-preview.nvim"},
-	{src = "https://github.com/altermo/ultimate-autopair.nvim"},
+  { src = "https://github.com/vague2k/vague.nvim" },
+  { src = "https://github.com/stevearc/oil.nvim" },
+  { src = "https://github.com/echasnovski/mini.pick" },
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/chomosuke/typst-preview.nvim" },
+  { src = "https://github.com/hrsh7th/nvim-cmp" },
+  { src = "https://github.com/hrsh7th/cmp-nvim-lsp" },
+  { src = "https://github.com/hrsh7th/cmp-buffer" },
+  { src = "https://github.com/hrsh7th/cmp-path" },
+  { src = "https://github.com/altermo/ultimate-autopair.nvim" },
+})
+
+local cmp = require("cmp")
+
+cmp.setup({
+  completion = {
+    completeopt = "menu,menuone,noselect",
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  }),
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "buffer" },
+    { name = "path" },
+  },
 })
 
 vim.lsp.enable({"clangd"})
+vim.lsp.enable({"zls"})
 vim.lsp.enable({"lua_ls"})
 
-require'ultimate-autopair'.setup{}
+require("ultimate-autopair").setup({})
 
 vim.cmd("colorscheme vague")
-vim.cmd(":hi statusline guibg=NONE")
+vim.cmd("hi statusline guibg=NONE")
+
